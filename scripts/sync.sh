f77ceeda574f75cf3a79f445128093f1f49a1ec6
moochi <voogie01+github@gmail.com>
2013-07-11 18:58:31 +0900

    change sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,5 +3,5 @@ svn co http://src.chromium.org/svn/trunk/tools/depot_tools
 export PATH="$PATH":`pwd`/depot_tools
 #gclient config http://mozc.googlecode.com/svn/trunk/src
 #gclient sync
-gclient config https://mocchi-mozc-android.googlecode.com/svn/
+gclient config --name src git+https://code.google.com/p/mocchi-mozc-android/ 
 gclient sync
