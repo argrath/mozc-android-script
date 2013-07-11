899d855be9f4eefed57371d93b4c322c4ec796ad
moochi <voogie01+github@gmail.com>
2013-07-11 14:15:16 +0900

    chenge my Repository

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,6 @@
 cd /home/vagrant
-svn co http://src.chromium.org/svn/trunk/tools/depot_tools
-export PATH="$PATH":`pwd`/depot_tools
-gclient config http://mozc.googlecode.com/svn/trunk/src
-gclient sync
+#svn co http://src.chromium.org/svn/trunk/tools/depot_tools
+#export PATH="$PATH":`pwd`/depot_tools
+#gclient config http://mozc.googlecode.com/svn/trunk/src
+#gclient sync
+git clone https://code.google.com/p/mocchi-mozc-android/ src
